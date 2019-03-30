486df68273ffa69c5bf9478e3a62fdd21a54ff2d
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-03-30 13:23:38 -0500

    Update to original_data

---
+++ original_sql_scripts/allstarappearances.sql
@@ -0,0 +1,91 @@
+/*
+-- Query: SELECT * 
+FROM allstarfull
+WHERE playerID IN ('correca01','colege01','harpebr03','strasst01','beckhti01','priceda01','hochelu01','uptonju01','bushma01','youngde03','bullibr01','mauerjo01','gonzaad01','hamiljo03','burrepa01','anderma01','erstada01','wilsopa02','rodrial01','nevinph01','jonesch06','mcdonbe01','benesan01','griffke02','kingje01','surhobj01','abnersh01','belchti01','dunstsh01','mooremi01','strawda01','chambal01')
+ORDER BY playerID
+LIMIT 0, 50000
+
+-- Date: 2019-02-27 19:38
+*/
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('benesan01',1993,0,'ALS199307130','SDN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('colege01',2015,0,'NLS201507140','PIT','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('dunstsh01',1988,0,'NLS198807120','CHN','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('dunstsh01',1990,0,'NLS199007100','CHN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('erstada01',1998,0,'NLS199807070','ANA','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('erstada01',2000,0,'NLS200007110','ANA','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('gonzaad01',2008,0,'ALS200807150','SDN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('gonzaad01',2009,0,'NLS200907140','SDN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('gonzaad01',2010,0,'ALS201007130','SDN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('gonzaad01',2011,0,'NLS201107120','BOS','AL',1,'3');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('gonzaad01',2015,0,'NLS201507140','LAN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1990,0,'NLS199007100','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1991,0,'ALS199107090','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1992,0,'NLS199207140','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1993,0,'ALS199307130','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1994,0,'NLS199407120','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1995,0,'ALS199507110','SEA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1996,0,'NLS199607090','SEA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1997,0,'ALS199707080','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1998,0,'NLS199807070','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',1999,0,'ALS199907130','SEA','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',2000,0,'NLS200007110','CIN','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',2004,0,'NLS200407130','CIN','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('griffke02',2007,0,'NLS200707100','CIN','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('hamiljo03',2008,0,'ALS200807150','TEX','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('hamiljo03',2009,0,'NLS200907140','TEX','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('hamiljo03',2010,0,'ALS201007130','TEX','AL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('hamiljo03',2011,0,'NLS201107120','TEX','AL',1,'7');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('hamiljo03',2012,0,'ALS201207100','TEX','AL',1,'7');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('harpebr03',2012,0,'ALS201207100','WAS','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('harpebr03',2013,0,'NLS201307160','WAS','NL',1,'8');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('harpebr03',2015,0,'NLS201507140','WAS','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('harpebr03',2016,0,'ALS201607120','WAS','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',1996,0,'NLS199607090','ATL','NL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',1997,0,'ALS199707080','ATL','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',1998,0,'NLS199807070','ATL','NL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',2000,0,'NLS200007110','ATL','NL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',2001,0,'ALS200107100','ATL','NL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',2008,0,'ALS200807150','ATL','NL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',2011,0,'NLS201107120','ATL','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('jonesch06',2012,0,'ALS201207100','ATL','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mauerjo01',2006,0,'NLS200607110','MIN','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mauerjo01',2008,0,'ALS200807150','MIN','AL',1,'2');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mauerjo01',2009,0,'NLS200907140','MIN','AL',1,'2');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mauerjo01',2010,0,'ALS201007130','MIN','AL',1,'2');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mauerjo01',2012,0,'ALS201207100','MIN','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mauerjo01',2013,0,'NLS201307160','MIN','AL',1,'2');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('mooremi01',1989,0,'ALS198907110','OAK','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('nevinph01',2001,0,'ALS200107100','SDN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('priceda01',2010,0,'ALS201007130','TBA','AL',1,'1');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('priceda01',2011,0,'NLS201107120','TBA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('priceda01',2012,0,'ALS201207100','TBA','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('priceda01',2014,0,'ALS201407150','TBA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('priceda01',2015,0,'NLS201507140','DET','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',1996,0,'NLS199607090','SEA','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',1997,0,'ALS199707080','SEA','AL',1,'6');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',1998,0,'NLS199807070','SEA','AL',1,'6');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2000,0,'NLS200007110','SEA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2001,0,'ALS200107100','TEX','AL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2002,0,'NLS200207090','TEX','AL',1,'6');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2003,0,'ALS200307150','TEX','AL',1,'6');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2004,0,'NLS200407130','NYA','AL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2005,0,'ALS200507120','NYA','AL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2006,0,'NLS200607110','NYA','AL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2007,0,'NLS200707100','NYA','AL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2008,0,'ALS200807150','NYA','AL',1,'5');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2010,0,'ALS201007130','NYA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('rodrial01',2011,0,'NLS201107120','NYA','AL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strasst01',2012,0,'ALS201207100','WAS','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strasst01',2016,0,'ALS201607120','WAS','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1984,0,'NLS198407100','NYN','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1985,0,'ALS198507160','NYN','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1986,0,'NLS198607150','NYN','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1987,0,'ALS198707140','NYN','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1988,0,'NLS198807120','NYN','NL',1,'9');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1989,0,'ALS198907110','NYN','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1990,0,'NLS199007100','NYN','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('strawda01',1991,0,'ALS199107090','LAN','NL',0,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('surhobj01',1999,0,'ALS199907130','BAL','AL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('uptonju01',2009,0,'NLS200907140','ARI','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('uptonju01',2011,0,'NLS201107120','ARI','NL',1,'');
+INSERT INTO `` (`playerID`,`yearID`,`gameNum`,`gameID`,`teamID`,`lgID`,`GP`,`startingPos`) VALUES ('uptonju01',2015,0,'NLS201507140','SDN','NL',1,'');
