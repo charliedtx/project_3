ff2b896fb03596c941e7b402537f9ccd906c77f3
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-04-03 21:35:15 -0500

    test

--- baseball/app.py
+++ baseball/app.py
@@ -69,7 +69,7 @@ def batting():
 @app.route("/api/attendance")
 def attendance():
     print('attendance!', file=sys.stderr)
-    results = db.session.query(HomeGames.team, func.sum(HomeGames.attendance)).group_by(HomeGames.year).all()
+    results = db.session.query(HomeGames.team, func.sum(HomeGames.attendance)).all()
     print('attendance!', file=sys.stderr)
 
     team = [result[0] for result in results]
