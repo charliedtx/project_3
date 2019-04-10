9cfaf70bbadbae73fe6c9b5591a2f1d0ac77bfdb
gfbelmarez <gfbelmarez@yahoo.com>
2019-04-09 21:55:39 -0500

    made fix to dropdown

--- baseball/static/js/app.js
+++ baseball/static/js/app.js
@@ -13,76 +13,111 @@ function buildPlot() {
 
 function buildHomerunPlot() {
   /* data route */
-var url = "/api/homeruns";
-d3.json(url).then(function(response) {
-  var data = response;
-  var layout = {
-            "title": "Homeruns per Year"
-  }
-
-  Plotly.newPlot("plot", [data], layout);
-});
+  var url = "/api/homeruns";
+  d3.json(url).then(function(response) {
+    var data = response;
+    var layout = {
+      title: "Homeruns per Year",
+      xaxis: {
+        tickangle: -45
+      },
+      annotations: [
+        {
+          x: 1981,
+          y: 26609610,
+          xref: 'x',
+          yref: 'y',
+          text: '81 Player Strike',
+          showarrow: true,
+          arrowhead: 6,
+          ax: 70,
+          ay: 0
+        },
+        {
+          x: 1994,
+          y: 49984410,
+          xref: 'x',
+          yref: 'y',
+          text: '94-95 Baseball Lockout Season',
+          showarrow: true,
+          arrowhead: 6,
+          ax: 0,
+          ay: 50
+        }
+      ]
+    };
+    Plotly.newPlot("plot", [data], layout);
+  });
 }
 
 function buildAttendancePlot() {
   /* data route */
-var url = "/api/attendance";
-d3.json(url).then(function(response) {
-  var data = response;
-  var layout = {
-          title: 'Yearly Attendance for MLB (by Team)',
-          xaxis: {
-                  tickangle: -45
-          },
-          annotations: [
-    {
-      x: 1981,
-      y: 26609610,
-      xref: 'x',
-      yref: 'y',
-      text: '81 Player Strike',
-      showarrow: true,
-      arrowhead: 6,
-      ax: 70,
-      ay: 0
-    },
-    {
-      x: 1994,
-      y: 49984410,
-      xref: 'x',
-      yref: 'y',
-      text: '94-95 Baseball Lockout Season',
-      showarrow: true,
-      arrowhead: 6,
-      ax: 0,
-      ay: 50
-    }
-  ]
-  };
-  Plotly.newPlot("plot", [data], layout);
-});
+  var url = "/api/attendance";
+  d3.json(url).then(function(response) {
+    var data = response;
+    var layout = {
+      title: 'Yearly Attendance for MLB (by Team)',
+      xaxis:  {
+              tickangle: -45
+              },
+      annotations: [
+      {
+        x: 1981,
+        y: 26609610,
+        xref: 'x',
+        yref: 'y',
+        text: '81 Player Strike',
+        showarrow: true,
+        arrowhead: 6,
+        ax: 70,
+        ay: 0
+      },
+      {
+        x: 1994,
+        y: 49984410,
+        xref: 'x',
+        yref: 'y',
+        text: '94-95 Baseball Lockout Season',
+        showarrow: true,
+        arrowhead: 6,
+        ax: 0,
+        ay: 50
+      }
+    ]
+    };
+    Plotly.newPlot("plot", [data], layout);
+  });
 }
 
 function buildSalariesPlot() {
   /* data route */
-var url = "/api/salaries";
-d3.json(url).then(function(response) {
-  var data = response;
+  var url = "/api/salaries";
+  d3.json(url).then(function(response) {
+    var data = response;
 
-  Plotly.newPlot("plot", [data]);
-});
+    Plotly.newPlot("plot", [data]);
+  });
 }
 
 d3.selectAll(".dropdown-item").on("click", function(){
   switch (this.textContent){
     case "@plot yearly-attendance":
+      d3.selectAll("img").remove();
       buildAttendancePlot();
       break;
     case "@plot homeruns":
+      d3.selectAll("img").remove();
       buildHomerunPlot();
       break;
     case "@plot salaries":
+      d3.selectAll("img").remove();
       buildSalariesPlot();
+      break;
+    case "@plot runsPerGame":
+      Plotly.purge('plot');
+      d3.selectAll("img").remove();
+      d3.select("#plot").append('img')
+      .attr('src','../static/img/RplotRunsPerGameAnnotated.png')
       break;
     default:
       buildPlot();
